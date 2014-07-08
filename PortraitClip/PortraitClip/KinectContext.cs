f525fc5d5c1dfdb97b24b54d23a83fc777fc4808
Keiho Sakapon <github@saka-pon.net>
2014-07-08 19:22:04 +0900

    Add PortraitTracker class

--- PortraitClip/PortraitClip/KinectContext.cs
+++ PortraitClip/PortraitClip/KinectContext.cs
@@ -1,6 +1,7 @@
 ﻿using KLibrary.ComponentModel;
 using Microsoft.Kinect;
 using Microsoft.Kinect.Toolkit;
+using Microsoft.Kinect.Toolkit.BackgroundRemoval;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -23,13 +24,19 @@ namespace PortraitClip
         public KinectSensorChooser SensorChooser
         {
             get { return GetValue<KinectSensorChooser>(); }
-            set { SetValue(value); }
+            private set { SetValue(value); }
         }
 
         public KinectSensor Sensor
         {
             get { return GetValue<KinectSensor>(); }
-            set { SetValue(value); }
+            private set { SetValue(value); }
+        }
+
+        public BackgroundRemovedColorStream BackgroundRemovedColorStream
+        {
+            get { return GetValue<BackgroundRemovedColorStream>(); }
+            private set { SetValue(value); }
         }
 
         public KinectContext()
@@ -54,6 +61,11 @@ namespace PortraitClip
         {
             try
             {
+                if (BackgroundRemovedColorStream != null)
+                {
+                    BackgroundRemovedColorStream.Dispose();
+                    BackgroundRemovedColorStream = null;
+                }
                 if (SensorChooser.Status == ChooserStatus.SensorStarted)
                 {
                     SensorChooser.Stop();
@@ -64,8 +76,109 @@ namespace PortraitClip
             }
         }
 
+        event EventHandler<AllFramesReadyEventArgs> allFramesReady = (o, e) => { };
+        public event EventHandler<AllFramesReadyEventArgs> AllFramesReady
+        {
+            add
+            {
+                allFramesReady += value;
+                if (Sensor != null)
+                {
+                    Sensor.AllFramesReady += value;
+                }
+            }
+            remove
+            {
+                allFramesReady -= value;
+                if (Sensor != null)
+                {
+                    Sensor.AllFramesReady -= value;
+                }
+            }
+        }
+
+        event EventHandler<BackgroundRemovedColorFrameReadyEventArgs> backgroundRemovedFrameReady = (o, e) => { };
+        public event EventHandler<BackgroundRemovedColorFrameReadyEventArgs> BackgroundRemovedFrameReady
+        {
+            add
+            {
+                backgroundRemovedFrameReady += value;
+                if (BackgroundRemovedColorStream != null)
+                {
+                    BackgroundRemovedColorStream.BackgroundRemovedFrameReady += value;
+                }
+            }
+            remove
+            {
+                backgroundRemovedFrameReady -= value;
+                if (BackgroundRemovedColorStream != null)
+                {
+                    BackgroundRemovedColorStream.BackgroundRemovedFrameReady -= value;
+                }
+            }
+        }
+
         void SensorChooser_KinectChanged(object sender, KinectChangedEventArgs e)
         {
+            if (e.OldSensor != null)
+            {
+                try
+                {
+                    if (BackgroundRemovedColorStream != null)
+                    {
+                        BackgroundRemovedColorStream.BackgroundRemovedFrameReady -= backgroundRemovedFrameReady;
+                        BackgroundRemovedColorStream.Dispose();
+                        BackgroundRemovedColorStream = null;
+                    }
+
+                    e.OldSensor.AllFramesReady -= allFramesReady;
+                    e.OldSensor.ColorStream.Disable();
+                    e.OldSensor.DepthStream.Disable();
+                    e.OldSensor.SkeletonStream.Disable();
+                }
+                catch (InvalidOperationException)
+                {
+                    // KinectSensor might enter an invalid state while enabling/disabling streams or stream features.
+                    // E.g.: sensor might be abruptly unplugged.
+                }
+            }
+
+            if (e.NewSensor != null)
+            {
+                try
+                {
+                    e.NewSensor.ColorStream.Enable(ColorImageFormat.RgbResolution640x480Fps30);
+                    e.NewSensor.DepthStream.Enable(DepthImageFormat.Resolution320x240Fps30);
+                    e.NewSensor.SkeletonStream.Enable();
+
+                    try
+                    {
+                        e.NewSensor.DepthStream.Range = DepthRange.Near;
+                        e.NewSensor.SkeletonStream.EnableTrackingInNearRange = true;
+                    }
+                    catch (InvalidOperationException)
+                    {
+                        // Non Kinect for Windows devices do not support Near mode, so reset back to default mode.
+                        e.NewSensor.DepthStream.Range = DepthRange.Default;
+                        e.NewSensor.SkeletonStream.EnableTrackingInNearRange = false;
+                    }
+                    e.NewSensor.SkeletonStream.TrackingMode = SkeletonTrackingMode.Seated;
+
+
+                    BackgroundRemovedColorStream = new BackgroundRemovedColorStream(e.NewSensor);
+                    BackgroundRemovedColorStream.Enable(ColorImageFormat.RgbResolution640x480Fps30, DepthImageFormat.Resolution320x240Fps30);
+
+                    e.NewSensor.AllFramesReady += allFramesReady;
+                    BackgroundRemovedColorStream.BackgroundRemovedFrameReady += backgroundRemovedFrameReady;
+                }
+                catch (InvalidOperationException)
+                {
+                    // KinectSensor might enter an invalid state while enabling/disabling streams or stream features.
+                    // E.g.: sensor might be abruptly unplugged.
+                }
+            }
+
+            Sensor = e.NewSensor;
         }
     }
 }
---
+++ PortraitClip/PortraitClip/PortraitTracker.cs
@@ -0,0 +1,94 @@
+﻿using KLibrary.ComponentModel;
+using Microsoft.Kinect;
+using Microsoft.Kinect.Toolkit.BackgroundRemoval;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace PortraitClip
+{
+    public class PortraitTracker : NotifyBase
+    {
+        public WriteableBitmap ClipBitmap
+        {
+            get { return GetValue<WriteableBitmap>(); }
+            private set { SetValue(value); }
+        }
+
+        Skeleton[] skeletons;
+
+        public PortraitTracker()
+        {
+            KinectContext.Current.AllFramesReady += Context_AllFramesReady;
+            KinectContext.Current.BackgroundRemovedFrameReady += BackgroundRemovedFrameReady;
+        }
+
+        void Context_AllFramesReady(object sender, AllFramesReadyEventArgs e)
+        {
+            var backgroundRemovedColorStream = KinectContext.Current.BackgroundRemovedColorStream;
+            if (backgroundRemovedColorStream == null) return;
+
+            try
+            {
+                using (var cf = e.OpenColorImageFrame())
+                {
+                    if (cf != null)
+                    {
+                        backgroundRemovedColorStream.ProcessColor(cf.GetRawPixelData(), cf.Timestamp);
+                    }
+                }
+
+                using (var df = e.OpenDepthImageFrame())
+                {
+                    if (df != null)
+                    {
+                        backgroundRemovedColorStream.ProcessDepth(df.GetRawPixelData(), df.Timestamp);
+                    }
+                }
+
+                using (var sf = e.OpenSkeletonFrame())
+                {
+                    if (sf != null)
+                    {
+                        if (skeletons == null)
+                        {
+                            skeletons = new Skeleton[sf.SkeletonArrayLength];
+                        }
+                        sf.CopySkeletonDataTo(skeletons);
+                        backgroundRemovedColorStream.ProcessSkeleton(skeletons, sf.Timestamp);
+
+                        var skeleton = skeletons
+                            .Where(s => s.TrackingState == SkeletonTrackingState.Tracked)
+                            .OrderBy(s => s.Position.Z)
+                            .FirstOrDefault();
+                        if (skeleton != null)
+                        {
+                            backgroundRemovedColorStream.SetTrackedPlayer(skeleton.TrackingId);
+                        }
+                    }
+                }
+            }
+            catch (InvalidOperationException)
+            {
+            }
+        }
+
+        void BackgroundRemovedFrameReady(object sender, BackgroundRemovedColorFrameReadyEventArgs e)
+        {
+            using (var bf = e.OpenBackgroundRemovedColorFrame())
+            {
+                if (bf != null)
+                {
+                    if (ClipBitmap == null)
+                    {
+                        ClipBitmap = new WriteableBitmap(bf.Width, bf.Height, 96.0, 96.0, PixelFormats.Bgra32, null);
+                    }
+                    ClipBitmap.WritePixels(new Int32Rect(0, 0, ClipBitmap.PixelWidth, ClipBitmap.PixelHeight), bf.GetRawPixelData(), 4 * ClipBitmap.PixelWidth, 0);
+                }
+            }
+        }
+    }
+}
