ecc82aa98ae9ca60bd54aba0d7e00bc6fb1db03f
Keiho Sakapon <github@saka-pon.net>
2014-07-08 12:46:46 +0900

    Update project settings

--- PortraitClip/PortraitClip/Properties/AssemblyInfo.cs
+++ PortraitClip/PortraitClip/Properties/AssemblyInfo.cs
@@ -7,12 +7,12 @@ using System.Windows;
 // アセンブリに関する一般情報は以下の属性セットをとおして制御されます。
 // アセンブリに関連付けられている情報を変更するには、
 // これらの属性値を変更してください。
-[assembly: AssemblyTitle("PortraitClip")]
+[assembly: AssemblyTitle("Portrait Clip")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("PortraitClip")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCompany("Keiho Sakapon")]
+[assembly: AssemblyProduct("Portrait Clip")]
+[assembly: AssemblyCopyright("© 2014 Keiho Sakapon")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
